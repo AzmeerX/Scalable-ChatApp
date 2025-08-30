138dca51d36521b931beb48fb1f0ff54954f8fd1
Ghost <azmeerfarhan786@gmail.com>
2025-08-30 22:12:35 +0500

    added conversation schema

---
+++ src/models/conversation.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const conversationSchema = mongoose.Schema(
+    {
+        user1: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        user2: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
