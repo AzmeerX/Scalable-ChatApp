ed46bf4edce4e81c94d0de45306efaa715728f42
Ghost <azmeerfarhan786@gmail.com>
2025-09-06 11:23:38 +0500

    added routes for update msg status and mark all msgs seen

--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/auth.middleware.js";
-import { getMessages, sendMessage } from "../controllers/message.controller.js";
+import { getMessages, markAllSeen, sendMessage, updateMessageStatus } from "../controllers/message.controller.js";
 
 const router = Router();
 
 router.route("/send-message").post(verifyToken, sendMessage);
 
 router.route("/:conversationId").get(verifyToken, getMessages);
+
+router.route("/:conversationId/status").patch(verifyToken, updateMessageStatus);
+
+router.route("/:conversationId/seen").patch(verifyToken, markAllSeen);
+
 
 export default router;
